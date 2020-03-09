840d73f163d553ff9ce9d0bcbbf18650039c0c89
p.piscione <p.piscione@nextworks.it>
2020-03-09 09:50:20 +0100

    Implemented mechanism to check the differences between configuration file and startup datastore. Test with wss and tpa YANG models.

--- netconf_server_netopeer/startup_config_diff.py
+++ netconf_server_netopeer/startup_config_diff.py
@@ -3,78 +3,129 @@ import xmltodict, json
 import copy
 
 ET.register_namespace("", "urn:cesnet:tmc:datastores:file")
-#ET.register_namespace("tpa", "http://org/nextworks/qameleon/tpa")
-
-def get_startup_datastore_file(datastore_filename, yang_model_name):
-	f = open(datastore_filename, "r")
-	data_store_str=f.read()
-	datastore_startup_xml = ET.fromstring(data_store_str)
-	datastore_startup_json = xmltodict.parse(data_store_str)		
-	#startup_config=datastore_startup_json["datastores"]["startup"][yang_model_name]##TODO if no key found, means no startup config
-	#startup_config=datastore_startup_json
-	#print(json.dumps(startup_config, indent=4, sort_keys=True))
-	return datastore_startup_xml, datastore_startup_json
 
 
-def get_configuration_from_file(filename):
+
+#TODO when the file format and the parameter is available, modify the implementation accordingly.		
+def get_configuration_from_file_tpa(filename):
 	print("Pretending to read from "+filename+"configuration file");
 	params = {}
-	params["tpa-id"]="1"
-	params["port-id"]="1"
-	params["n"]="10"
-	params["m"]="20"
+	params["tpa-id"]="44"
+	params["port-id"]="44"
+	params["n"]="44"
+	params["m"]="44"
 	print("params:")
 	print(params)
 	return params
 
 
-	
-def find_diff(datastore_startup_xml, params_from_file, object_name):
-	#Cases:
-	#1. The configuration parameters are equal. No action performed.
-	#2. The configuration parameters are different:
-		#2.1. No startup datastore 
-		#2.2 Different parameter (Generic case)
-		#2.3 Not available parameters
-		#2.4 Other cases ??
-	root=datastore_startup_xml
-	found=False
-	
-	for child in root:
-		if "startup" in child.tag:
-			startup_root=child
-
-	startup_root_tmp = copy.deepcopy(startup_root)
-
-	for elem in startup_root_tmp.iter():	
-		for key in params_from_file.keys():
-			str_tmp=elem.tag.split("}")[1]
-			if(key == str_tmp and params_from_file[key]==elem.text):
-				print("value of "+key+" are equal:"+elem.text)
-			if(key == str_tmp and params_from_file[key]!=elem.text):
-				print("value of "+key+" are different: XML datastore startup value: "+elem.text+ ". Configuration file value: "+params_from_file[key])
-				elem.text=params_from_file[key]
+#TODO when the file format and the parameter is available, modify the implementation accordingly.
+def get_configuration_from_file_wss(filename):
+	print("Pretending to read from "+filename+" configuration file.");
+	params = {}
+	params["wss-id"]="44"
+	params["port-id"]="44"
+	params["n"]="45"
+	params["m"]="45"
+	print("params:")
+	print(params)
+	return params
 
 
-	for elem in startup_root.iter():
-		print(elem.tag, elem.text)
 
-	print("...........................")
-	for elem in startup_root_tmp.iter():
-		print(elem.tag, elem.text)
-	
- 
+def get_startup_datastore_file(datastore_filename, yang_model_name):
+	try:
+		f = open(datastore_filename, "r")
+		data_store_str=f.read()
+		datastore_startup_xml = ET.fromstring(data_store_str)
+		return datastore_startup_xml
+
+	except OSError:
+		print("File "+datastore_filename+ "not available. ")
+		sys.exit()
+
+
+def write_conf_file(datastore_startup_xml, object_name):
+	#Before writing on the new configuration file, the xml string is cleaned
 	xmlstr = ET.tostring(datastore_startup_xml, method='xml').decode('utf8')
 	xmlstr="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+xmlstr
 	my_str=xmlstr.replace("ns1:","")
 	my_str=my_str.replace("<"+object_name+">","<"+object_name+" xmlns=\"http://org/nextworks/qameleon/"+object_name+"\">")
 	my_str=my_str.replace(" xmlns:ns1=\"http://org/nextworks/qameleon/"+object_name+"\"","")
 	my_str+="\n"
-	text_file = open("datastore-"+object_name+"_tmp.xml", "w")
+	text_file = open("datastore-"+object_name+".xml", "w")
 	n = text_file.write(my_str)
 	text_file.close()
+	
+def find_diff(datastore_startup_xml, params_from_file, object_name):
+	#Cases:
+	#1. The configuration parameters are equal. No action performed.
+	#2. The configuration parameters are different:
+		#2.1 File available, but no startup datastore 
+		#2.2 Different parameters (Generic case)
 
-params_from_file=get_configuration_from_file("tpa_conf.txt")
-datastore_startup_xml, datastore_startup_json=get_startup_datastore_file("datastore-tpa.xml","tpa")
-find_diff(datastore_startup_xml, params_from_file,"tpa")
+
+	root=datastore_startup_xml
+	found=False
+	
+	for child in root:
+		if "startup" in child.tag:
+			startup_root=child
+	
+
+	startup_root_tmp = copy.deepcopy(startup_root)
+	
+	#TODO There would be a set of minimal configuration that should be available at the start up time
+	# Figure out what these are (for tpa, wss and so on) and set it when the below case occurs
+	if not hasattr(startup_root_tmp,'child'):#2.1 File available, but no startup datastore 
+		print("Startup xml has no child. It means no startup configuration")
+		return
+
+	print(ET.tostring(startup_root_tmp, method='xml').decode('utf8'))
+	conf_are_equal=True
+
+	for elem in startup_root_tmp.iter():	
+		for key in params_from_file.keys():
+			str_tmp=elem.tag.split("}")[1]
+			if(key == str_tmp and params_from_file[key]==elem.text):
+				print("value of "+key+" are equal:"+elem.text)
+			if(key == str_tmp and params_from_file[key]!=elem.text):#Case #2.2
+				print("value of "+key+" are different: XML datastore startup value: "+elem.text+ ". Configuration file value: "+params_from_file[key])
+				elem.text=params_from_file[key]
+				conf_are_equal=False
+	
+	if(conf_are_equal):#Case #1.
+		print("Configuration file not changed. Not going to update datastore file for "+object_name)
+		return
+		
+	
+	for child in datastore_startup_xml:
+		if "startup" in child.tag:
+			datastore_startup_xml.remove(child)
+			break;
+
+	datastore_startup_xml.append(startup_root_tmp)		
+	
+	write_conf_file(datastore_startup_xml, object_name)
+
+
+def test_tpa():
+	object_name="tpa"
+	datastore_filename="datastore-"+object_name+".xml";
+	params_from_file=get_configuration_from_file_tpa(object_name+"_conf.txt")
+	datastore_startup_xml= get_startup_datastore_file(datastore_filename,object_name)
+	find_diff(datastore_startup_xml, params_from_file,object_name)
+
+
+def test_wss():
+	object_name="wss"
+	datastore_filename="datastore-"+object_name+".xml";
+	params_from_file=get_configuration_from_file_wss(object_name+"_conf.txt")
+	datastore_startup_xml= get_startup_datastore_file(datastore_filename,object_name)
+	find_diff(datastore_startup_xml, params_from_file,object_name)
+
+
+test_tpa()
+#test_wss()
+
 
