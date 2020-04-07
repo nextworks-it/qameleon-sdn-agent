52ea3f370b0017a52a7f3b6e886766ff8de2f607
p.piscione <p.piscione@nextworks.it>
2020-04-07 17:03:56 +0200

    deleted remote repository mistakenly added.

--- docker/netopeer/cov-gen-tgz.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if test "$1" = ""; then
-	echo "Requires an argument - the path to \"cov-build\""
-	exit 1
-fi
-
-./cov-configure.sh
-$1 --dir cov-int ./cov-make.sh
-tar czvf netopeer.tgz cov-int
